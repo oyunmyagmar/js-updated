d90396870e65b458d518b0275b0f47c146d5e604
oyunmyagmar <25LP4018@nest.edu.mn>
2025-07-22 03:13:59 +0900

    class review

--- script-star.js
+++ script-star.js
@@ -1,4 +1,11 @@
 // 1. Square Star Pattern
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
 let sp1Size = 7;
 let sp1mur = sp1Size;
 
@@ -14,6 +21,13 @@ while (sp1mur > 0) {
 }
 
 // 2. Hollow Square Star Pattern
+
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
 let sp2Size = 8;
 let sp2row = sp2Size;
 
@@ -38,12 +52,13 @@ while (sp2row > 0) {
 }
 
 // 3. Hollow Square Star Pattern with Diagonal
-//  12345
-//1 *****
-//2 **  *  2 - 2
-//3 * * *  3 - 3
-//4 *  **  4 - 4
-//5 *****
+
+// *****
+// **  *
+// * * *
+// *  **
+// *****
+
 let sp3Size = 6;
 let sp3row = sp3Size;
 
@@ -68,17 +83,19 @@ while (sp3row > 0) {
   sp3row = sp3row - 1;
 }
 
-// start of - Hollow Square Star Pattern with LEFT Diagonal
+// 3-1. Hollow Square Star Pattern with LEFT Diagonal
+
 //  |
 //  V
 //  column
-//  12345
-//1 *****
-//2 *  **  2 - 4
-//3 * * *  3 - 3
-//4 **  *  4 - 2
+//  54321
 //5 *****
+//4 *  **  4 - 2
+//3 * * *  3 - 3
+//2 **  *  2 - 4
+//1 *****
 // -> row
+
 let size = 5;
 let row = size;
 while (row > 0) {
@@ -103,18 +120,63 @@ while (row > 0) {
 }
 // end of - Hollow Square Star Pattern with LEFT Diagonal
 
-// 11. Inverted Right Triangle Star Pattern
-//  12345
-//1 *****
-//2 ****
-//3 ***
-//4 **
-//5 *
+// 9. Mirrored Right Triangle Star Pattern
 
+//  12345
+//1     * 1-1
+//2    ** 2-2
+//3   *** 3-3
+//4  **** 4-4
+//5 ***** 5-5
+
+let sp9size = 5;
+let sp9row = 1;
+while (sp9row <= sp9size) {
+  let sp9column = 1;
+  let sp9Result = "";
+  while (sp9column <= sp9size - sp9row) {
+    sp9Result = sp9Result + " ";
+    sp9column = sp9column + 1;
+  }
+  let starCount = sp9row;
+  while (starCount > 0) {
+    sp9Result = sp9Result + "*";
+    starCount = starCount + 1;
+  }
+  console.log(sp9Result);
+  sp9row = sp9row + 1;
+}
+
+// 11. Inverted Right Triangle Star Pattern
+
+//  54321
+//5 *****
+//4 ****
+//3 ***
+//2 **
+//1 *
+
+// option 1
+let s11Size = 5;
+let s11row = s11Size;
+while (s11row > 0) {
+  let s11column = s11Size;
+  let s11Result = "";
+  while (s11column > 0) {
+    if (s11row >= s11column) {
+      s11Result = s11Result + "*";
+    }
+    s11column = s11column - 1;
+  }
+  console.log(s11Result);
+  s11row = s11row - 1;
+}
+
+// option 2
 let sp11Size = 5;
 let sp11row = sp11Size;
 while (sp11row > 0) {
-  let sp11column = sp11Size;
+  let sp11column = sp11row;
   let sp11Result = "";
   while (sp11column > 0) {
     sp11Result = sp11Result + "*";
@@ -122,4 +184,26 @@ while (sp11row > 0) {
   }
   console.log(sp11Result);
   sp11row = sp11row - 1;
+}
+
+// option 3
+
+//  12345
+//1 ***** 1-5
+//2 ****  2-4
+//3 ***   3-3
+//4 **    4-2
+//5 *     5-1
+
+let size11 = 5;
+let row11 = 1;
+while (row11 <= size11) {
+  let column11 = 1;
+  let Result11 = "";
+  while (column11 <= size11 - row11 + 1) {
+    Result11 = Result11 + "*";
+    column11 = column11 + 1;
+  }
+  console.log(Result11);
+  row11 = row11 + 1;
 }
