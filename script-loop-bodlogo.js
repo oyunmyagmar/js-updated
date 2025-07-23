007a3a65b63251cdda635a5a905c4772b80e2989
oyunmyagmar <25LP4018@nest.edu.mn>
2025-07-23 20:46:09 +0900

    save at dom

--- script-loop-bodlogo.js
+++ script-loop-bodlogo.js
@@ -1,16 +1,18 @@
+// 1. 1-ээс n хүртэлх тоонуудыг хэвлэ
 let numInput = document.getElementById("num-input");
+let exponentInput = document.getElementById("exponent-input");
 
 let allNumBtn = document.getElementById("all-num-btn");
 allNumBtn.addEventListener("click", () => {
   let num = numInput.value;
-  //   console.log(num);
-  //   console.log(typeof num);
+  // console.log(num);
+  // console.log(typeof num);
   let number = parseInt(num);
-  //   console.log(typeof number);
+  // console.log(typeof number);
 
   document.getElementById("answer").innerText = "";
   while (number > 0) {
-    console.log(number); //10 9 8 7 6 5 4 3 2 1
+    // console.log(number); //10 9 8 7 6 5 4 3 2 1
     document.getElementById("answer").innerText =
       document.getElementById("answer").innerText + "\n" + number;
     number = number - 1; //9 8 7 6 5 4 3 2 1 0
@@ -23,6 +25,7 @@ allNumBtn.addEventListener("click", () => {
 //   too = too - 1; //9 8 7 6 5 4 3 2 1 0
 // }
 
+//2. 1-ээс n хүртэлх тэгш тоог хэвлэ
 let evenNumBtn = document.getElementById("even-num-btn");
 evenNumBtn.addEventListener("click", () => {
   let num = numInput.value;
@@ -31,7 +34,7 @@ evenNumBtn.addEventListener("click", () => {
   document.getElementById("answer").innerText = "";
   while (number > 0) {
     if (number % 2 === 0) {
-      console.log(number);
+      // console.log(number);
       document.getElementById("answer").innerText =
         document.getElementById("answer").innerText + "\n" + number;
     }
@@ -39,6 +42,7 @@ evenNumBtn.addEventListener("click", () => {
   }
 });
 
+// 3. 1-ээс n хүртэлх тоонуудын нийлбэрийг ол
 let sumAllNumBtn = document.getElementById("sum-all-num-btn");
 sumAllNumBtn.addEventListener("click", () => {
   let num = numInput.value;
@@ -49,12 +53,13 @@ sumAllNumBtn.addEventListener("click", () => {
   while (number > 0) {
     // console.log(number);
     sum = sum + number;
-    console.log(sum);
+    // console.log(sum);
     document.getElementById("answer").innerText = sum;
     number = number - 1;
   }
 });
 
+// 4. 1-ээс n хүртэлх сондгой тоонуудын нийлбэрийг ол.
 let sumOddNumBtn = document.getElementById("sum-odd-num-btn");
 sumOddNumBtn.addEventListener("click", () => {
   let num = numInput.value;
@@ -66,14 +71,15 @@ sumOddNumBtn.addEventListener("click", () => {
     if (number % 2 === 1) {
       //   console.log(number);
       sum = sum + number;
-      console.log(sum);
+      // console.log(sum);
       document.getElementById("answer").innerText = sum;
     }
-
     number = number - 1;
   }
 });
 
+// 5. n тооны үржвэрийн хүрд хэвлэ
+// ж: input:5 1x5=5 2x5=10 3x5=15 4x5=20 5x5=25 6x5=30 7x5=35 8x5=40 9x5=45
 let timesTableBtn = document.getElementById("times-table-btn");
 timesTableBtn.addEventListener("click", () => {
   let num = numInput.value;
@@ -85,7 +91,7 @@ timesTableBtn.addEventListener("click", () => {
   document.getElementById("answer").innerText = "";
   while (range > 0) {
     timesTable = number * i;
-    console.log(timesTable);
+    // console.log(timesTable);
     document.getElementById("answer").innerText =
       document.getElementById("answer").innerText +
       "\n" +
@@ -94,13 +100,54 @@ timesTableBtn.addEventListener("click", () => {
       i +
       "=" +
       timesTable;
-    range = range - 1;
     i = i + 1;
+    range = range - 1;
   }
 });
 
+// 6. Тоо хэдэн оронтойг тоолох програм бич.
 let countDigitBtn = document.getElementById("count-digit-btn");
+countDigitBtn.addEventListener("click", () => {
+  let num = numInput.value;
+  let number = parseInt(num);
+  let i = 0;
+
+  document.getElementById("answer").innerText = "";
+  while (number > 0) {
+    number = number / 10;
+    i = i + 1;
+    number = number - 1;
+  }
+  // console.log(i);
+  document.getElementById("answer").innerText = i;
+});
+
+// 7. Эхний болон сүүлийн оронг ол
 let firstLastDigitBtn = document.getElementById("first-last-digit-btn");
+firstLastDigitBtn.addEventListener("click", () => {
+  let num = numInput.value;
+  let number = parseInt(num);
+  let firstDigit = 0;
+  let lastDigit = 0;
+  lastDigit = number % 10;
+  // console.log("Last digit: " + lastDigit);
+
+  document.getElementById("answer").innerText = "";
+  while (number > 10) {
+    number = number / 10;
+    firstDigit = parseInt(number);
+  }
+  // console.log("First digit: " + firstDigit);
+  document.getElementById("answer").innerText =
+    document.getElementById("answer").innerText +
+    "\n" +
+    "First digit: " +
+    firstDigit +
+    "\n" +
+    "Last digit: " +
+    lastDigit;
+});
+
 let sumFirstLastDigitBtn = document.getElementById("sum-first-last-digit-btn");
 let swapFirstLastDigitBtn = document.getElementById(
   "swap-first-last-digit-btn"
