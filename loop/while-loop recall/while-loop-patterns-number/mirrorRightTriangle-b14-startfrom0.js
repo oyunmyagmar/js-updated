6cb0e4fea4fcda278da1680198d19ec406507909
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-03 17:16:58 +0900

    number loop

--- loop/while-loop recall/while-loop-patterns-number/mirrorRightTriangle-b14-startfrom0.js
+++ loop/while-loop recall/while-loop-patterns-number/mirrorRightTriangle-b14-startfrom0.js
@@ -5,3 +5,18 @@
 //3 --123
 //4 -1234
 //5 12345
+
+let size = 5;
+let row = 1;
+
+while (row < size) {
+  let stars = "";
+  let column = 1;
+
+  while (column < size) {
+    stars = stars + "*";
+    column = column + 1;
+  }
+  console.log(stars);
+  row = row + 1;
+}
