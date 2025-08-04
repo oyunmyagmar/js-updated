56161973e0f563bf7d122e3dcf444c875a563166
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-04 21:19:56 +0900

    nodejs review

--- a-lesson-at-class-8.4.js
+++ a-lesson-at-class-8.4.js
@@ -1,10 +1,41 @@
 // 8.4 nodejs
-// js ni
-// website -> inspect -> network <- tsana ashiglaj bui js bolon html -iig server-ees tataj bga
-// nodejs -> js browser -gui dangara ajillah bolomj olgoj bga zuiliig
+
+// js ni webpage web application hiihed zoriulsan hel
+// js -iin gants ajillah yostoi orchin ni browser gdeg zuil
+// browser -gui bol js ajillahgui
+// yamar 1 website -> inspect -> network <- tsana ashiglaj bui js bolon html -iig server-ees tataj bga
+
+// nodejs -> js -iig browser -gui dangara ajillah bolomj olgoj bga zuiliig
 
 const a = 5;
 const b = 4;
 console.log(a + b);
 // eniig browser -gui ajilluulj bui zuiliig nodejs gene
 // bidnii ajilluulj bsn code runner extension ni nodejs der suurilsan belen software
+
+// nodejs -iig herhen yaj ashiglah ve?
+// terminal deeree -> node -v ged version -iig checkleh
+// js gile -iig ajilluulahdaa -> node index.js
+
+// nodejs -iig package manager buyu npm gedg zuiltei horshulj ajillana
+// npm -> package -uud buyu yamar 1 zuil hideg belen sanguud bga <- teriig zohitsuldag sulgadag ustgadag zuiliig helne
+
+// tgvel yaj npm gedgiig ashiglad humuusiin hisen bga sanguudiig ashiglah ve?
+// ehled vscode terminal der npm init command ugnu <- package -iinhaa stack -iig beldene
+
+// npm init -> doorh zuil garch irne
+// package name? -> ugch bolno esvel enter
+// software description? -> ugch bolno
+// enter file ? -> ene package -iin hamgin ehend ajilluulah file -iig zaaj ugdug
+// test command ? -> bhgui bolohor enter
+// ...
+// duusgaad enter darval package.json gdeg zuil uusne
+// package.json dotor ni bidnii say bichsen zuils bn
+// "author" deer uursdiin neree bichij bolno
+
+// package.json dotor "scripts" gsen hseg bga
+// "scripts" -> package.json -iig ajilluulahad blen bolgoson command -uudiig nerlene
+// end uursdiin js file -aa ajilluldag command -aa hine
+// "start" gj nerled ih bichigleltei command -iig 1 ugeer ajilluulj boldog bolgono
+
+// package
