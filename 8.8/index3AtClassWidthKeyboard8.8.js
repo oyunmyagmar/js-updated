949416296478c539ac2eacd9637805a311bbe57f
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-09 21:09:09 +0900

    last update

--- 8.8/index3AtClassWidthKeyboard8.8.js
+++ 8.8/index3AtClassWidthKeyboard8.8.js
@@ -13,7 +13,8 @@ boxRed.style.width = "50%";
 boxBlue.style.width = "50%";
 
 document.addEventListener("keydown", function (event) {
-  //   console.log("key value of the pressed key: ", event.key);
+  // console.log(event);
+  // console.log("key value of the pressed key: ", event.key);
   if (event.key == "Enter") {
     boxRed.style.width = parseInt(boxRed.style.width) + 1 + "%";
     boxBlue.style.width = parseInt(boxBlue.style.width) - 1 + "%";
@@ -34,17 +35,25 @@ body.appendChild(boxBlue);
 
 // "keydown" -> keyboard der darah action -> keyboard -nii yun der ch darsan ajillana
 // keyboard barij avah ni yamar ch element -tei holbogui uchir document. gj bichne
+// syntax der event gdeg zuil avdag
+// event ?
+// console.log(event);
+// 1 keyboard darahad -> KeyboardEvent gdeg zuil garj irj bn
+// keyboard -nii ymar tochlur der darsan be gdeg medelel garch bn
+
+// if short syntax -> event.key == "a" ? x++ : y++;
+// short syntax der 1 l uildel hideg
+// if short syntax -> event.key == "a" ? x++ : y--; <- 2 zereg uildel hij blohgui
+// iim tohioldold if else -ee hereglene
 
 // !!!bichleg uzehgui documentation unshih!!!
 // " " -> hooson string
 
-// GPT-5
-// Taskmaster AI -> tom task -iig jijig bolgoj zadaldag
-
 // !!!documentation unshij surah!!!
 // bichleg bitgii uz
-// QDD approach to learning programming -> turshij uzeh
-// surah arga baril
+// QDD approach to learning programming -> turshij uzeh - decomposition <- surah arga baril
 
 // javascript 30 days
 // javascript documentation
+// beginner problems in javascript
+// beginner exercises in javascript
