ab2f0de1cfd1c9bf7a7b46c661acbb20597e063c
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-10 01:25:17 +0900

    js

---
+++ 30DaysJs/day1/variables.js
@@ -0,0 +1,14 @@
+let firstName = "Elle";
+let lastName = "Hugh";
+let country = "Hungary";
+let city = "City";
+let age = 30;
+let isMarried = true;
+
+const gravity = 9.81;
+const boilingPoint = 100;
+const PI = 3.14;
+
+let name = "Anna",
+  job = "teacher",
+  live = "Korea";
---
+++ devRoadmap/var.js
@@ -0,0 +1,40 @@
+let message = "hello";
+message = "hi";
+console.log(message);
+
+let msg = message;
+console.log(msg, message);
+
+let userName = "miga";
+console.log(userName);
+
+let userName123 = "davaa";
+console.log(userName123);
+
+let userName_ = "huslee";
+console.log(userName_);
+
+let userName$ = "bataa";
+console.log(userName$);
+
+let user = "boldoo";
+console.log(user);
+
+let USER = "ganaa";
+console.log(USER);
+
+// !!! reserved names !!!
+
+const myName = "myagmar";
+console.log(myName);
+
+// tasks1
+let admin;
+let name;
+name = "John";
+admin = name;
+console.log(admin);
+
+// tasks2
+let ourPlanetName = "Earth";
+let currentVisitorName = "todoo";
