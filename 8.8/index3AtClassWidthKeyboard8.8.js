8989c579b8e4716a06edad917284a503feb14f56
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-09 15:48:19 +0900

    lesson review 8.8

--- 8.8/index3AtClassWidthKeyboard8.8.js
+++ 8.8/index3AtClassWidthKeyboard8.8.js
@@ -20,7 +20,7 @@ document.addEventListener("keydown", function (event) {
 
     boxRed.innerText = boxRed.style.width;
     boxBlue.innerText = boxBlue.style.width;
-  } else if ((event.key = " ")) {
+  } else if (event.key == " ") {
     boxBlue.style.width = parseInt(boxBlue.style.width) + 1 + "%";
     boxRed.style.width = parseInt(boxRed.style.width) - 1 + "%";
 
@@ -31,3 +31,19 @@ document.addEventListener("keydown", function (event) {
 
 body.appendChild(boxRed);
 body.appendChild(boxBlue);
+
+// "keydown" -> keyboard der darah action -> keyboard -nii yun der ch darsan ajillana
+// keyboard barij avah ni yamar ch element -tei holbogui uchir document. gj bichne
+
+// !!!bichleg uzehgui documentation unshih!!!
+// " " -> hooson string
+
+// GPT-5
+// Taskmaster AI -> tom task -iig jijig bolgoj zadaldag
+// gpt -> code -iin task muu
+// gtp -> research bolon idea hiihed zoriulsan
+// gto -> yaj surah talaar asuj bolno <- hel saijirna
+
+// !!!documentation unshij surah!!!
+// bichleg bitgii uz
+// QDD approach tp learning programming -> turshij uzeh
