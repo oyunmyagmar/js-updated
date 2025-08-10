d56be90824ba6736aa5f046babaee5be6664443f
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-10 19:20:50 +0900

    third.js

---
+++ 7.7/second.js
@@ -0,0 +1,35 @@
+// ugugdliin turul, mouse-aa deer ni avaachihad yu bn gedeg ni haragdana
+// number -> too
+let age = 20;
+console.log(2025 - age);
+
+// string -> ug uguulber, usegnuudiin tsugluulga, "ehleltei "tugsgultei
+let name = "miga";
+
+// boolean -> computer -iin logic unen hudal, computer 0 1 -iig l oilgodog
+// false -> hudal 0
+// true -> unen 1
+let graduated = false;
+
+// string uchras string der yamar 1 too nemeh gej oroldvol araas ni zalgulj bichdeg
+// yer ni bol blohgui neg ni too neg ni string
+let num1 = 10;
+let num2 = "10";
+console.log(num1 + num2);
+
+// too bsan bol nemj bolno
+let num3 = 10;
+let num4 = 10;
+console.log(num3 + num4);
+
+// celcuis ugugdsun
+// farenheit gsen huvisagch zarla
+// farenheit dotorh utgaa bodoj olno, ugugdsun celcuis huvisagch -aa ashiglana
+// bodoj olsnoo console.log der bichih
+let celcuis = 100;
+let farenheit = celcuis * (9 / 5) + 32;
+console.log(farenheit);
+
+let cel = 100;
+let far = cel * 1.8 + 32;
+console.log(far);
---
+++ 7.7/third.js
@@ -0,0 +1,62 @@
+//opearators + - * / %
+
+// string -tai haritsaj bga bol + ees uur uildel hij blohgui
+
+// bolohgui
+let num1 = "10";
+let num2 = 10;
+console.log(num1 - num2);
+
+let num3 = 40;
+let num4 = 50;
+console.log(num3 + num4);
+console.log(num3 - num4);
+console.log(num3 * num4);
+console.log(num3 / num4);
+
+let sum = num3 + num4;
+console.log(sum);
+
+let sub = num3 - num4;
+console.log(sub);
+
+let multiplication = num3 * num4;
+console.log(multiplication);
+
+let division = num3 / num4;
+console.log(division);
+
+// % module -> uldegdel
+let remainNum3 = num3 % 3;
+console.log(remainNum3);
+
+let remainNum4 = num4 % 7;
+console.log(remainNum4);
+
+let remain = 20 % 6;
+console.log(remain);
+
+let r1 = 5 % 2;
+console.log(r1);
+
+let r2 = 11 % 3;
+console.log(r2);
+
+// % -> yamar 1 too tegsh esvel sondgoig todorhoilj bolno
+// yaj ashiglaj bloh ve?
+// ehnii mur ni tsagaan
+// daragin mur ni saaral ungutei ghed hereglej bolno
+
+// assignment operator
+// assignment operator = ni math-iin tentsuu temdeg bish
+// = assignment operator -iin barun tald bga utgiig zuun tald bga huvisagch -d orulj irne
+let n = 10;
+let n1 = 11;
+let nIsBig = n > n1;
+console.log(nIsBig);
+
+let a = 10;
+a = 20; // utgiig ni uurchilj bga
+let b = 11;
+let aIsBig = a >= b;
+console.log(aIsBig);
