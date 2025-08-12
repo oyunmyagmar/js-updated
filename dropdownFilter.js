1670466c7e2a271e6fbd0204bec4cee3771690c6
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-12 13:51:17 +0800

    number5bodlogo

--- dropdownFilter.js
+++ dropdownFilter.js
@@ -1,11 +1,37 @@
 const body = document.querySelector("body");
 
 const title = document.createElement("h5");
-const select = document.createElement("select");
-const option = document.createTextNode("option");
+const select = document.getElementById("food-select");
+const ul = document.createElement("ul");
 
 title.innerText = "5) Dropdown Filter";
 
-select.append(option);
+let food = [
+  { name: "Apple", category: "Fruits" },
+  { name: "Banana", category: "Fruits" },
+  { name: "Bluberry", category: "Fruits" },
+  { name: "Pea", category: "Vegetables" },
+  { name: "Potato", category: "Vegetables" },
+  { name: "Broccoli", category: "Vegetables" },
+  { name: "Milk", category: "Dairy" },
+  { name: "Yogurt", category: "Dairy" },
+  { name: "Cheese", category: "Dairy" },
+];
+
+food.map((element) => {
+  const li = document.createElement("li");
+  li.innerText += element.name;
+  ul.appendChild(li);
+});
+
+const printFood = () => {
+  food.map((element) => {
+    const li = document.createElement("li");
+    li.innerText += element.name;
+    ul.appendChild(li);
+  });
+};
+// select.append(option);
 body.appendChild(title);
-body.appendChild(select);
+body.appendChild(ul);
+// body.appendChild(select);
