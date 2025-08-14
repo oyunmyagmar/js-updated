d1c90361a12c541c85c90346e1012670a7dcef2d
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-15 04:00:04 +0900

    athome

--- $8.14/questionnaireAtClass.js
+++ $8.14/questionnaireAtClass.js
@@ -5,15 +5,15 @@ const correctAnswer = document.createElement("p");
 
 let questionsArr = [
   { q: "Question number1", a: ["ans1", "ans2", "ans", "ans4"], c: "ans1" },
-  { q: "Question number2", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans2" },
-  { q: "Question number3", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans3" },
-  { q: "Question number4", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans4" },
+  { q: "Question number2", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans4" },
+  { q: "Question number3", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans2" },
+  { q: "Question number4", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans3" },
   { q: "Question number5", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans2" },
 ];
 
 let count = 0;
 const printQuestions = () => {
-  const question = document.createElement("p");
+  const question = document.createElement("h3");
   question.innerText = questionsArr[count].q;
 
   //   console.log(questionsArr[0]);
