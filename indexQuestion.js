109358fc15d2f5fffccb5569c9900f7d8e07ca47
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-14 12:35:22 +0800

    atclass

--- indexQuestion.js
+++ indexQuestion.js
@@ -1,41 +1,66 @@
+let customerAnswerArr = [];
 const body = document.querySelector("body");
 const btn = document.getElementById("btn");
+const correctAnswer = document.createElement("p");
 
 let questionsArr = [
-  { q: "Question number1", a: ["1", "2", "3", "4"], c: "1" },
-  { q: "Question number2", a: ["1", "2", "3", "4"], c: "2" },
-  { q: "Question number3", a: ["1", "2", "3", "4"], c: "3" },
-  { q: "Question number4", a: ["1", "2", "3", "4"], c: "4" },
-  { q: "Question number5", a: ["1", "2", "3", "4"], c: "1" },
+  { q: "Question number1", a: ["ans1", "ans2", "ans", "ans4"], c: "ans1" },
+  { q: "Question number2", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans2" },
+  { q: "Question number3", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans3" },
+  { q: "Question number4", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans4" },
+  { q: "Question number5", a: ["ans1", "ans2", "ans3", "ans4"], c: "ans2" },
 ];
 
+let count = 0;
 const printQuestions = () => {
-  let count = 0;
-  const p = document.createElement("p");
-  p.innerText = questionsArr[count].q;
+  const question = document.createElement("p");
+  question.innerText = questionsArr[count].q;
 
-  console.log(questionsArr[0]);
+  //   console.log(questionsArr[0]);
 
   const btn1 = document.createElement("button");
-  btn1.innerText = questionsArr[0].a[0];
+  btn1.innerText = questionsArr[count].a[0];
   const btn2 = document.createElement("button");
-  btn2.innerText = questionsArr[0].a[1];
+  btn2.innerText = questionsArr[count].a[1];
   const btn3 = document.createElement("button");
-  btn3.innerText = questionsArr[0].a[2];
+  btn3.innerText = questionsArr[count].a[2];
   const btn4 = document.createElement("button");
-  btn4.innerText = questionsArr[0].a[3];
+  btn4.innerText = questionsArr[count].a[3];
+
+  changePrintQuestionValue = () => {
+    count++;
+    question.innerText = questionsArr[count].q;
+    btn1.innerText = questionsArr[count].a[0];
+    btn2.innerText = questionsArr[count].a[1];
+    btn3.innerText = questionsArr[count].a[2];
+    btn4.innerText = questionsArr[count].a[3];
+  };
 
   btn1.addEventListener("click", () => {
     // body.innerHTML = "";
-    count++;
-    printQuestions();
+    // console.log(questionsArr[count]);
+
+    // console.log(btn1.innerText);
+    customerAnswerArr.push(btn1.innerText);
+    // console.log(customerAnswerArr);
+
+    if (customerAnswerArr[0] == questionsArr[count].c) {
+      correctAnswer.innerText += customerAnswerArr[0];
+    } else {
+      correctAnswer.innerText += questionsArr[count].c;
+    }
+    console.log("customerAnswerArr[0]", customerAnswerArr[0]);
+    console.log("questionsArr[count].c", questionsArr[count].c);
+    // console.log(correctAnswer);
+    changePrintQuestionValue();
   });
 
-  body.appendChild(p);
+  body.appendChild(question);
   body.appendChild(btn1);
   body.appendChild(btn2);
   body.appendChild(btn3);
   body.appendChild(btn4);
+  body.appendChild(correctAnswer);
 };
 
 btn.addEventListener("click", () => {
