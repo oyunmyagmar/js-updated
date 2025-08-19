634dd3818fd37519392b3b9e70dfd3a89741eedd
25LP4018 25LP4018 <25LP4018@Setups-iMac.localdomain>
2025-08-19 18:46:35 +0800

    finished calculator

--- augustArvanNegen/indexTipCalculator.js
+++ augustArvanNegen/indexTipCalculator.js
@@ -1,18 +1,14 @@
 const body = document.querySelector("body");
-
 const tipCalculatorContainer = document.createElement("div");
 const tipCalculatorTitle = document.createElement("h1");
 const mainCalculator = document.createElement("div");
-
 const billContainer = document.createElement("div");
 const billTitle = document.createElement("p");
 const billInput = document.createElement("input");
-
 const tipSelectorContainer = document.createElement("div");
 const tipSelectorTitle = document.createElement("p");
-const tipBtnSection = document.createElement("div");
-
-let total = document.createElement("p");
+const tipBtnsSection = document.createElement("div");
+const total = document.createElement("p");
 const resetBtn = document.createElement("button");
 
 tipCalculatorContainer.classList.add("tipCalculatorContainer");
@@ -23,30 +19,36 @@ billTitle.classList.add("billTitle");
 billInput.classList.add("billInput");
 tipSelectorContainer.classList.add("tipSelectorContainer");
 tipSelectorTitle.classList.add("tipSelectorTitle");
-tipBtnSection.classList.add("tipBtnSection");
-
+tipBtnsSection.classList.add("tipBtnsSection");
 total.classList.add("total");
 resetBtn.classList.add("resetBtn");
 
 tipCalculatorTitle.innerText = "Tip Calculator";
 billTitle.innerText = "Bill";
-billInput.placeholder = "$";
+billInput.placeholder = "₮";
 tipSelectorTitle.innerText = "Select Tip %";
-
-total.innerText = "Total: ";
+total.innerText = "Total";
 resetBtn.innerText = "Reset";
 
-[5, 10, 15, 25, 35, 50, 65, 80].map((element) => {
+[5, 10, 15, 25, 35, 50, 65, 80].forEach((element) => {
   const btn = document.createElement("button");
   btn.classList.add("btn");
-  tipBtnSection.appendChild(btn);
   btn.innerText = element + "%";
-});
 
-// tip5PerBtn.addEventListener("click", () => {
-//   total.innerText = "Total: ";
-//   total.innerText += parseFloat(billInput.value * 1.05).toFixed(2);
-// });
+  btn.addEventListener("click", () => {
+    if (billInput.value !== "") {
+      total.innerText = "Total";
+      const tippedTotalBill = parseFloat(
+        parseInt(billInput.value) * (1 + element / 100)
+      ).toFixed(2);
+      total.innerText = `Total ₮ ${tippedTotalBill} `;
+    } else {
+      billInput.value = "";
+    }
+  });
+
+  tipBtnsSection.appendChild(btn);
+});
 
 resetBtn.addEventListener("click", () => {
   total.innerText = "0.00";
@@ -55,16 +57,12 @@ resetBtn.addEventListener("click", () => {
 
 billContainer.appendChild(billTitle);
 billContainer.appendChild(billInput);
-
 tipSelectorContainer.appendChild(tipSelectorTitle);
-tipSelectorContainer.appendChild(tipBtnSection);
-
+tipSelectorContainer.appendChild(tipBtnsSection);
 mainCalculator.appendChild(billContainer);
 mainCalculator.appendChild(tipSelectorContainer);
 mainCalculator.appendChild(total);
 mainCalculator.appendChild(resetBtn);
-
 tipCalculatorContainer.appendChild(tipCalculatorTitle);
 tipCalculatorContainer.appendChild(mainCalculator);
-
 body.appendChild(tipCalculatorContainer);
