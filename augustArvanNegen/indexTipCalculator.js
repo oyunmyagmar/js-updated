2c07e4a731ed238faea2ca405833b0d67980cbbc
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-20 20:11:00 +0900

    tip

--- augustArvanNegen/indexTipCalculator.js
+++ augustArvanNegen/indexTipCalculator.js
@@ -1,14 +1,26 @@
 const body = document.querySelector("body");
+
 const tipCalculatorContainer = document.createElement("div");
 const tipCalculatorTitle = document.createElement("h1");
 const mainCalculator = document.createElement("div");
+
 const billContainer = document.createElement("div");
 const billTitle = document.createElement("p");
 const billInput = document.createElement("input");
+
 const tipSelectorContainer = document.createElement("div");
 const tipSelectorTitle = document.createElement("p");
-const tipBtnsSection = document.createElement("div");
-const total = document.createElement("p");
+const tipBtns = document.createElement("div");
+const tip5PerBtn = document.createElement("button");
+const tip10PerBtn = document.createElement("button");
+const tip15PerBtn = document.createElement("button");
+const tip25PerBtn = document.createElement("button");
+const tip35PerBtn = document.createElement("button");
+const tip50PerBtn = document.createElement("button");
+const tip65PerBtn = document.createElement("button");
+const tip80PerBtn = document.createElement("button");
+
+let total = document.createElement("p");
 const resetBtn = document.createElement("button");
 
 tipCalculatorContainer.classList.add("tipCalculatorContainer");
@@ -19,35 +31,65 @@ billTitle.classList.add("billTitle");
 billInput.classList.add("billInput");
 tipSelectorContainer.classList.add("tipSelectorContainer");
 tipSelectorTitle.classList.add("tipSelectorTitle");
-tipBtnsSection.classList.add("tipBtnsSection");
+tipBtns.classList.add("tipBtns");
+tip5PerBtn.classList.add("btn");
+tip10PerBtn.classList.add("btn");
+tip15PerBtn.classList.add("btn");
+tip25PerBtn.classList.add("btn");
+tip35PerBtn.classList.add("btn");
+tip50PerBtn.classList.add("btn");
+tip65PerBtn.classList.add("btn");
+tip80PerBtn.classList.add("btn");
 total.classList.add("total");
 resetBtn.classList.add("resetBtn");
 
 tipCalculatorTitle.innerText = "Tip Calculator";
+
 billTitle.innerText = "Bill";
-billInput.placeholder = "₮";
+billInput.placeholder = "$";
 tipSelectorTitle.innerText = "Select Tip %";
-total.innerText = "Total";
+tip5PerBtn.innerText = "5%";
+tip10PerBtn.innerText = "10%";
+tip15PerBtn.innerText = "15%";
+tip25PerBtn.innerText = "25%";
+tip35PerBtn.innerText = "35%";
+tip50PerBtn.innerText = "50%";
+tip65PerBtn.innerText = "65%";
+tip80PerBtn.innerText = "80%";
+total.innerText = "Total: ";
 resetBtn.innerText = "Reset";
 
-[5, 10, 15, 25, 35, 50, 65, 80].forEach((element) => {
-  const btn = document.createElement("button");
-  btn.classList.add("btn");
-  btn.innerText = element + "%";
-
-  btn.addEventListener("click", () => {
-    if (billInput.value !== "") {
-      total.innerText = "Total";
-      const tippedTotalBill = parseFloat(
-        parseInt(billInput.value) * (1 + element / 100)
-      ).toFixed(2);
-      total.innerText = `Total ₮ ${tippedTotalBill} `;
-    } else {
-      billInput.value = "";
-    }
-  });
-
-  tipBtnsSection.appendChild(btn);
+tip5PerBtn.addEventListener("click", () => {
+  total.innerText = "Total: ";
+  total.innerText += parseFloat(billInput.value * 1.05).toFixed(2);
+});
+tip10PerBtn.addEventListener("click", () => {
+  total.innerText = "Total: ";
+  total.innerText += parseFloat(billInput.value * 1.1).toFixed(2);
+});
+tip15PerBtn.addEventListener("click", () => {
+  total.innerText = "Total: ";
+  total.innerText += parseFloat(billInput.value * 1.15).toFixed(2);
+});
+tip25PerBtn.addEventListener("click", () => {
+  total.innerText = "Total: ";
+  total.innerText += parseFloat(billInput.value * 1.25).toFixed(2);
+});
+tip35PerBtn.addEventListener("click", () => {
+  total.innerText = "Total: ";
+  total.innerText += parseFloat(billInput.value * 1.35).toFixed(2);
+});
+tip50PerBtn.addEventListener("click", () => {
+  total.innerText = "Total: ";
+  total.innerText += parseFloat(billInput.value * 1.5).toFixed(2);
+});
+tip65PerBtn.addEventListener("click", () => {
+  total.innerText = "Total: ";
+  total.innerText += parseFloat(billInput.value * 1.65).toFixed(2);
+});
+tip80PerBtn.addEventListener("click", () => {
+  total.innerText = "Total: ";
+  total.innerText += parseFloat(billInput.value * 1.8).toFixed(2);
 });
 
 resetBtn.addEventListener("click", () => {
@@ -57,12 +99,25 @@ resetBtn.addEventListener("click", () => {
 
 billContainer.appendChild(billTitle);
 billContainer.appendChild(billInput);
+
+tipBtns.appendChild(tip5PerBtn);
+tipBtns.appendChild(tip10PerBtn);
+tipBtns.appendChild(tip15PerBtn);
+tipBtns.appendChild(tip25PerBtn);
+tipBtns.appendChild(tip35PerBtn);
+tipBtns.appendChild(tip50PerBtn);
+tipBtns.appendChild(tip65PerBtn);
+tipBtns.appendChild(tip80PerBtn);
+
 tipSelectorContainer.appendChild(tipSelectorTitle);
-tipSelectorContainer.appendChild(tipBtnsSection);
+tipSelectorContainer.appendChild(tipBtns);
+
 mainCalculator.appendChild(billContainer);
 mainCalculator.appendChild(tipSelectorContainer);
 mainCalculator.appendChild(total);
 mainCalculator.appendChild(resetBtn);
+
 tipCalculatorContainer.appendChild(tipCalculatorTitle);
 tipCalculatorContainer.appendChild(mainCalculator);
+
 body.appendChild(tipCalculatorContainer);
--- projectDomTipCalculator/script.js
+++ projectDomTipCalculator/script.js
@@ -1,26 +1,14 @@
 const body = document.querySelector("body");
-
 const tipCalculatorContainer = document.createElement("div");
 const tipCalculatorTitle = document.createElement("h1");
 const mainCalculator = document.createElement("div");
-
 const billContainer = document.createElement("div");
 const billTitle = document.createElement("p");
 const billInput = document.createElement("input");
-
 const tipSelectorContainer = document.createElement("div");
 const tipSelectorTitle = document.createElement("p");
-const tipBtns = document.createElement("div");
-const tip5PerBtn = document.createElement("button");
-const tip10PerBtn = document.createElement("button");
-const tip15PerBtn = document.createElement("button");
-const tip25PerBtn = document.createElement("button");
-const tip35PerBtn = document.createElement("button");
-const tip50PerBtn = document.createElement("button");
-const tip65PerBtn = document.createElement("button");
-const tip80PerBtn = document.createElement("button");
-
-let total = document.createElement("p");
+const tipBtnsSection = document.createElement("div");
+const total = document.createElement("p");
 const resetBtn = document.createElement("button");
 
 tipCalculatorContainer.classList.add("tipCalculatorContainer");
@@ -31,65 +19,35 @@ billTitle.classList.add("billTitle");
 billInput.classList.add("billInput");
 tipSelectorContainer.classList.add("tipSelectorContainer");
 tipSelectorTitle.classList.add("tipSelectorTitle");
-tipBtns.classList.add("tipBtns");
-tip5PerBtn.classList.add("btn");
-tip10PerBtn.classList.add("btn");
-tip15PerBtn.classList.add("btn");
-tip25PerBtn.classList.add("btn");
-tip35PerBtn.classList.add("btn");
-tip50PerBtn.classList.add("btn");
-tip65PerBtn.classList.add("btn");
-tip80PerBtn.classList.add("btn");
+tipBtnsSection.classList.add("tipBtnsSection");
 total.classList.add("total");
 resetBtn.classList.add("resetBtn");
 
 tipCalculatorTitle.innerText = "Tip Calculator";
-
 billTitle.innerText = "Bill";
-billInput.placeholder = "$";
+billInput.placeholder = "₮";
 tipSelectorTitle.innerText = "Select Tip %";
-tip5PerBtn.innerText = "5%";
-tip10PerBtn.innerText = "10%";
-tip15PerBtn.innerText = "15%";
-tip25PerBtn.innerText = "25%";
-tip35PerBtn.innerText = "35%";
-tip50PerBtn.innerText = "50%";
-tip65PerBtn.innerText = "65%";
-tip80PerBtn.innerText = "80%";
-total.innerText = "Total: ";
+total.innerText = "Total";
 resetBtn.innerText = "Reset";
 
-tip5PerBtn.addEventListener("click", () => {
-  total.innerText = "Total: ";
-  total.innerText += parseFloat(billInput.value * 1.05).toFixed(2);
-});
-tip10PerBtn.addEventListener("click", () => {
-  total.innerText = "Total: ";
-  total.innerText += parseFloat(billInput.value * 1.1).toFixed(2);
-});
-tip15PerBtn.addEventListener("click", () => {
-  total.innerText = "Total: ";
-  total.innerText += parseFloat(billInput.value * 1.15).toFixed(2);
-});
-tip25PerBtn.addEventListener("click", () => {
-  total.innerText = "Total: ";
-  total.innerText += parseFloat(billInput.value * 1.25).toFixed(2);
-});
-tip35PerBtn.addEventListener("click", () => {
-  total.innerText = "Total: ";
-  total.innerText += parseFloat(billInput.value * 1.35).toFixed(2);
-});
-tip50PerBtn.addEventListener("click", () => {
-  total.innerText = "Total: ";
-  total.innerText += parseFloat(billInput.value * 1.5).toFixed(2);
-});
-tip65PerBtn.addEventListener("click", () => {
-  total.innerText = "Total: ";
-  total.innerText += parseFloat(billInput.value * 1.65).toFixed(2);
-});
-tip80PerBtn.addEventListener("click", () => {
-  total.innerText = "Total: ";
-  total.innerText += parseFloat(billInput.value * 1.8).toFixed(2);
+[5, 10, 15, 25, 35, 50, 65, 80].forEach((element) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = element + "%";
+
+  btn.addEventListener("click", () => {
+    if (billInput.value !== "") {
+      total.innerText = "Total";
+      const tippedTotalBill = parseFloat(
+        parseInt(billInput.value) * (1 + element / 100)
+      ).toFixed(2);
+      total.innerText = `Total ₮ ${tippedTotalBill} `;
+    } else {
+      billInput.value = "";
+    }
+  });
+
+  tipBtnsSection.appendChild(btn);
 });
 
 resetBtn.addEventListener("click", () => {
@@ -99,25 +57,12 @@ resetBtn.addEventListener("click", () => {
 
 billContainer.appendChild(billTitle);
 billContainer.appendChild(billInput);
-
-tipBtns.appendChild(tip5PerBtn);
-tipBtns.appendChild(tip10PerBtn);
-tipBtns.appendChild(tip15PerBtn);
-tipBtns.appendChild(tip25PerBtn);
-tipBtns.appendChild(tip35PerBtn);
-tipBtns.appendChild(tip50PerBtn);
-tipBtns.appendChild(tip65PerBtn);
-tipBtns.appendChild(tip80PerBtn);
-
 tipSelectorContainer.appendChild(tipSelectorTitle);
-tipSelectorContainer.appendChild(tipBtns);
-
+tipSelectorContainer.appendChild(tipBtnsSection);
 mainCalculator.appendChild(billContainer);
 mainCalculator.appendChild(tipSelectorContainer);
 mainCalculator.appendChild(total);
 mainCalculator.appendChild(resetBtn);
-
 tipCalculatorContainer.appendChild(tipCalculatorTitle);
 tipCalculatorContainer.appendChild(mainCalculator);
-
 body.appendChild(tipCalculatorContainer);
